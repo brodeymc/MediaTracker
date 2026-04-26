a4f63a2177b8bfd8d728797677542717f87b5f09
brodeymc <brodeymc@icloud.com>
2026-04-26 18:43:48 -0400

    Initial API Setup

---
+++ Program.cs
@@ -0,0 +1,33 @@
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+// app.UseHttpsRedirection();
+
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll",
+        policy => policy
+            .AllowAnyOrigin()
+            .AllowAnyMethod()
+            .AllowAnyHeader());
+});
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+
+app.UseCors("AllowAll");
+app.Run();

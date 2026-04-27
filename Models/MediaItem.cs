104930d0f8e223b0065719afd5d2b3efee91f5de
brodeymc <brodeymc@icloud.com>
2026-04-26 20:49:23 -0400

    Created media item class

---
+++ Models/MediaItem.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace MediaTrackerAPI.Models
+{
+    public class MediaItem
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string MediaType { get; set; } // Movie, Game, TV Show, Book, etc.
+        public string Status { get; set; }
+        public string Progress { get; set; }
+        public string Notes { get; set; }
+        public string ImageUrl { get; set; } // For custom image upload
+        public DateTime lastUpdated { get; set; } = DateTime.UtcNow;
+
+    }
+}

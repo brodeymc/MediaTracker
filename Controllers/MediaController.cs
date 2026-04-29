4bead0338ac90e93732732f9cbc4216ac62ae8cc
brodeymc <brodeymc@icloud.com>
2026-04-29 17:57:38 -0400

    added Delete, Update, GetById functions

--- Controllers/MediaController.cs
+++ Controllers/MediaController.cs
@@ -24,14 +24,56 @@ namespace MediaTrackerAPI.Controllers
             return Ok(items);
         }
 
+        [HttpGet("{id}")]
+        public IActionResult GetById(int id)
+        {
+            var item = context.MediaItems.Find(id);
+            if (item == null) { return NotFound(); }
+
+            return Ok(item);
+        }
+
         [HttpPost]
         public IActionResult Create(MediaItem item)
         {
-            context.MediaItems.Add(item);
             item.lastUpdated = DateTime.UtcNow;
+
+            context.MediaItems.Add(item);
             context.SaveChanges();
 
-            return Ok(item);
+            return CreatedAtAction(nameof(GetById), new {id = item.Id}, item);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Update(int id, MediaItem updatedItem)
+        {
+            var item = context.MediaItems.Find(id);
+
+            if (item == null) { return NotFound(); }
+
+            item.Title = updatedItem.Title;
+            item.MediaType = updatedItem.MediaType;
+            item.Status = updatedItem.Status;
+            item.Progress = updatedItem.Progress;
+            item.Notes = updatedItem.Notes;
+            item.ImageUrl  = updatedItem.ImageUrl;
+            item.lastUpdated = DateTime.UtcNow;
+
+            context.SaveChanges();
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            var item = context.MediaItems.Find(id);
+
+            if (item == null) { return NotFound(); }
+
+            context.MediaItems.Remove(item);
+            context.SaveChanges();
+
+            return NoContent() ;
         }
     }
 }
